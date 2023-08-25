a250356b1d7e102c2bcf06c74b6fec567809d932
Bernard-Thompson <thompsonb905@gmail.com>
2023-08-25 17:05:31 -0500

    This is an E-commerce application
    I wanted to see how fast I could spin up such an application

---
+++ Spring/BerniesDeals/src/main/java/com/deals/bernie/BerniesDealsApplication.java
@@ -0,0 +1,13 @@
+package com.deals.bernie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerniesDealsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BerniesDealsApplication.class, args);
+	}
+
+}
---
+++ Spring/BerniesDeals/src/test/java/com/deals/bernie/BerniesDealsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deals.bernie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BerniesDealsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
