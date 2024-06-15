6a1cb3801d8b145a0000e72b873e4238c29028d4
Bernard-Thompson <thompsonb905@gmail.com>
2024-06-15 16:07:02 -0500

    Removed application
    Turns out that the application name leads to some confusion compared to the application function. It will now be removed, renamed, and re-uploaded

--- TaskLife/src/main/java/com/bam/task/repository/AdventurerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bam.task.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bam.task.model.Adventurer;
-
-public interface AdventurerRepository extends JpaRepository<Adventurer, Long>{
-
-}
--- TaskLife/src/main/java/com/bam/task/repository/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bam.task.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bam.task.model.Client;
-
-public interface ClientRepository extends JpaRepository<Client, Long>{
-
-}
