4c0e1e7673a6b07e7846972ae4c310d8d7179c3f
Bernard-Thompson <thompsonb905@gmail.com>
2025-11-06 08:18:36 -0600

    Decentralized Identification Verification System
    SkillLedger: Personal Learning Tracker application

---
+++ SkillLedger/src/main/java/bam/skilllegder/SkillLedgerApplication.java
@@ -0,0 +1,13 @@
+package bam.skilllegder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillLedgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillLedgerApplication.class, args);
+	}
+
+}
---
+++ SkillLedger/src/test/java/bam/skilllegder/SkillLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package bam.skilllegder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillLedgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
